25b8e3e9fbc352470484218c134aa6d7911ce94a
Amélia Alice <alice.cardoso10@gmail.com>
2019-08-07 13:47:43 -0500

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/nelioalves/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,13 @@
+package com.nelioalves.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.domain.Cliente;
+import com.nelioalves.cursomc.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+	
+}
