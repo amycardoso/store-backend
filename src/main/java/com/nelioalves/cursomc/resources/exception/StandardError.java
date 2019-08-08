4ed41f703b8ebb89b9944fad2c49142fd11d3cfc
Amélia Alice <alice.cardoso10@gmail.com>
2019-08-08 13:15:04 -0500

    Personalizando o retorno da validação

--- src/main/java/com/nelioalves/cursomc/resources/exception/StandardError.java
+++ src/main/java/com/nelioalves/cursomc/resources/exception/StandardError.java
@@ -3,6 +3,8 @@ package com.nelioalves.cursomc.resources.exception;
 import java.io.Serializable;
 
 public class StandardError implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
 	private Integer status;
 	private String msg;
 	private Long timeStamp;
