6bcf3c55c7374852d00f56f742dcaf6d930d8f50
aafeitosa <alice.cardoso10@gmail.com>
2019-08-12 15:08:05 -0500

    Inserindo Pedido

--- src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
+++ src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
@@ -41,6 +41,14 @@ public class ItemPedido implements Serializable{
 		return id.getPedido();
 	}
 	
+	public void setPedido(Pedido pedido) {
+		id.setPedido(pedido);
+	}
+	
+	public void setProduto(Produto produto) {
+		id.setProduto(produto);
+	}
+	
 	public Produto getProduto() {
 		return id.getProduto();
 	}
--- src/main/java/com/nelioalves/cursomc/resources/PedidoResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/PedidoResource.java
@@ -1,16 +1,23 @@
 package com.nelioalves.cursomc.resources;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.nelioalves.cursomc.domain.Categoria;
 import com.nelioalves.cursomc.domain.Pedido;
+import com.nelioalves.cursomc.dto.CategoriaDTO;
 import com.nelioalves.cursomc.services.PedidoService;
 
 @RestController
@@ -24,4 +31,20 @@ public class PedidoResource {
 		Pedido obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 }
+
+
+
+
+
+
+
+
+
