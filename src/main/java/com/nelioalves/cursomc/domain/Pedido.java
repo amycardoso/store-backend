29104a704d112e530bf8cf3a1b14a784293134d8
aafeitosa <alice.cardoso10@gmail.com>
2019-08-12 14:32:00 -0500

    Subtotal e total do Pedido

--- src/main/java/com/nelioalves/cursomc/domain/Pedido.java
+++ src/main/java/com/nelioalves/cursomc/domain/Pedido.java
@@ -52,6 +52,14 @@ public class Pedido implements Serializable {
 		this.cliente = cliente;
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
+	
+	public double getValorTotal() {
+		double soma = 0.0;
+		for(ItemPedido ip : itens) {
+			soma += ip.getSubTotal();
+		}
+		return soma;
+	}
 
 	public Integer getId() {
 		return id;
