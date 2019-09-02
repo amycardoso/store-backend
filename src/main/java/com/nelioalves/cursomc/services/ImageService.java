59383b87b3a4d71aeae235ea8e9cf5286f21b45b
aafeitosa <alice.cardoso10@gmail.com>
2019-09-02 16:50:12 -0500

    Bonus: ajustando tamanho da imagem

--- src/main/java/com/nelioalves/cursomc/services/ImageService.java
+++ src/main/java/com/nelioalves/cursomc/services/ImageService.java
@@ -7,13 +7,14 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
- import javax.imageio.ImageIO;
+import javax.imageio.ImageIO;
 
- import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.imgscalr.Scalr;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
- import com.nelioalves.cursomc.services.exceptions.FileException;
+import com.nelioalves.cursomc.services.exceptions.FileException;
 
  @Service
 public class ImageService {
@@ -50,5 +51,19 @@ public class ImageService {
 		} catch (IOException e) {
 			throw new FileException("Erro ao ler arquivo");
 		}
+	}
+ 	
+ 	public BufferedImage cropSquare(BufferedImage sourceImg) {
+		int min = (sourceImg.getHeight() <= sourceImg.getWidth()) ? sourceImg.getHeight() : sourceImg.getWidth();
+		return Scalr.crop(
+			sourceImg, 
+			(sourceImg.getWidth()/2) - (min/2), 
+			(sourceImg.getHeight()/2) - (min/2), 
+			min, 
+			min);		
+	}
+
+ 	public BufferedImage resize(BufferedImage sourceImg, int size) {
+		return Scalr.resize(sourceImg, Scalr.Method.ULTRA_QUALITY, size);
 	}
 }
